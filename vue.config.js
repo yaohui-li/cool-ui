2199f80dbf4cdf16b687a5d69a98dd5fa8a2c573
liyaohui <65102243@qq.com>
2020-10-03 00:19:51 +0800

    添加文件

---
+++ vue.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+function resolve (dir) {
+  return path.resolve(__dirname, dir)
+}
+module.exports = {
+  productionSourceMap: false,
+  css: {
+    extract: false
+  },
+  pages: {
+    index: {
+      entry: 'examples/main.js',
+      template: 'public/index.html',
+      filename: 'index.html'
+    }
+  },
+  configureWebpack: {
+    output: {
+      libraryExport: 'default'
+    },
+    resolve: {
+      extensions: ['.js', '.vue', '.json'],
+      alias: {
+        '@': resolve('packages'),
+        assets: resolve('examples/assets'),
+        views: resolve('examples/views')
+      }
+    },
+    devServer: {
+      port: 1680,
+      hot: true,
+      open: 'Google Chrome'
+    }
+  }
+}
